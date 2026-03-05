6256c16e6666bf7a2c702cff690685222c123051
Ankit Raj <ankit188raj@gmail.com>
2026-03-05 16:13:22 +0530

    Files added

--- src/db.ts
+++ src/db.ts
@@ -1,25 +1,44 @@
-import mongoose from "mongoose";
+import mongoose, {model, Schema} from "mongoose";
+import { setServers } from "node:dns/promises";
+setServers(["1.1.1.1", "8.8.8.8"]);
 
-const userSchema=new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true
-    },
-    password:{
-        type:String,
-        required:true
+const getEnv=(key:string):string=>{
+    const value=process.env[key]  ;
+    if(value===undefined){
+        throw new Error(`Missing environment variable ${key}`)
     }
-});
-
-const User=mongoose.model("User",userSchema);
-
-export {
-    User,
-    
+    return value;
 }
+
+const MONGO_URL=getEnv("MONGO_URL");
+
+mongoose.connect(MONGO_URL)
+.then(()=>{
+    console.log("Database connect")
+})
+.catch((e)=>{
+    console.log(e)
+})
+
+const UserSchema = new Schema({
+    username: {type: String, unique: true},
+    password: String
+})
+
+export const UserModel = model("User", UserSchema);
+
+const ContentSchema = new Schema({
+    title: String,
+    link: String,
+    tags: [{type: mongoose.Types.ObjectId, ref: 'Tag'}],
+    type: String,
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true },
+})
+
+const LinkSchema = new Schema({
+    hash: String,
+    userId: {type: mongoose.Types.ObjectId, ref: 'User', required: true, unique: true },
+})
+
+export const LinkModel = model("Links", LinkSchema);
+export const ContentModel = model("Content", ContentSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,78 +1,175 @@
 import express from "express";
-import {z} from "zod";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();
-import dns from "node:dns/promises";
-import bcrypt from "bcrypt";
+import "dotenv/config";
+import { random } from "./utils.js";
 import jwt from "jsonwebtoken";
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
-import {User} from "./db.js";
+import { ContentModel, LinkModel, UserModel } from "./db.js";
+import { userMiddleware } from "./middleware.js";
+import cors from "cors";
+import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGO_URL!)
-.then(()=>{
-    console.log("Mongoose connection sucessful");
-})
-.catch((e)=>{
-    console.log("Error caught");
-    console.log(e);
-})
-
-const app=express();
-
+const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/",(req,res)=>{
     res.json({
-        msg:"Helo"
+        msg:"Hello there"
     })
-});
+})
 
-app.post("/signup",async (req,res)=>{
-    const {username,email,password} =req.body;
-    const requiredBody=z.object({
-        username:z.string(),
-        email:z.email(),
-        password:z.string()
-    });
-    const success=requiredBody.safeParse(req.body);
-    if(!success){
+app.post("/api/v1/signup", async (req, res) => {
+    // TODO: zod validation , hash the password
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        await UserModel.create({
+            username: username,
+            password: password
+        }) 
+
         res.json({
-            msg:"coundnt login"
+            message: "User signed up"
         })
-    }else{
-        const hashedPassword=await bcrypt.hash(password,5);
-        await User.create({
-            username:username,
-            email:email,
-            password:hashedPassword
+    } catch(e) {
+        console.log(e);
+        res.status(411).json({
+            message: "User already exists"
         })
-        res.json({
-            msg: "hi login successful",
-            
-        });
     }
 })
 
-app.post("/signin",async (req,res)=>{
-    const {username,email,password}=req.body;
-    const user=await User.findOne({
-        username:username
-    });
-    const checkUnhashedPassword=await bcrypt.compare(password,user!.password);
-    if(!checkUnhashedPassword){
-        res.status(401).json({
-            msg:"Wrong password or username"
-        });
-    }else{
-        const token=jwt.sign({
-            username:username,
-            date:new Date()
-        },process.env.JWT_SECRET!);
+app.post("/api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if (existingUser) {
+        const token = jwt.sign({
+            id: existingUser._id
+        }, process.env.JWT_SECRET!)
+
         res.json({
             token
         })
+    } else {
+        res.status(403).json({
+            message: "Incorrrect credentials"
+        })
     }
+})
+
+app.post("/api/v1/content", userMiddleware, async (req, res) => {
+    const link:string = req.body.link;
+    const type:string = req.body.type;
+    await ContentModel.create({
+        link,
+        type,
+        title: req.body.title,
+        userId: new mongoose.Types.ObjectId(req.userId as string),
+        tags: []
+    })
+
+    res.json({
+        message: "Content added"
+    })
+    
+})
+
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
+    const userId = new mongoose.Types.ObjectId(req.userId as string);
+    const content = await ContentModel.find({
+        userId: userId
+    }).populate("userId", "username")
+    res.json({
+        content
+    })
+})
+
+app.delete("/api/v1/content", userMiddleware, async (req, res) => {
+    const contentId = req.body.contentId;
+
+    await ContentModel.deleteMany({
+        _id: contentId,
+        userId: new mongoose.Types.ObjectId(req.userId as string)
+    })
+
+    res.json({
+        message: "Deleted"
+    })
+})
+
+app.post("/api/v1/brain/share", userMiddleware, async (req, res) => {
+    const share = req.body.share;
+    if (share) {
+            const existingLink = await LinkModel.findOne({
+                userId: new mongoose.Types.ObjectId(req.userId as string)
+            });
+
+            if (existingLink) {
+                res.json({
+                    hash: existingLink.hash
+                })
+                return;
+            }
+            const hash = random(10);
+            await LinkModel.create({
+                userId: new mongoose.Types.ObjectId(req.userId as string),
+                hash: hash
+            })
+
+            res.json({
+                hash
+            })
+    } else {
+        await LinkModel.deleteOne({
+            userId: new mongoose.Types.ObjectId(req.userId as string)
+        });
+
+        res.json({
+            message: "Removed link"
+        })
+    }
+})
+
+app.get("/api/v1/brain/:shareLink", async (req, res) => {
+    const hash = req.params.shareLink;
+
+    const link = await LinkModel.findOne({
+        hash
+    });
+
+    if (!link) {
+        res.status(411).json({
+            message: "Sorry incorrect input"
+        })
+        return;
+    }
+    // userId
+    const content = await ContentModel.find({
+        userId: link.userId
+    })
+
+    console.log(link);
+    const user = await UserModel.findOne({
+        _id: link.userId
+    })
+
+    if (!user) {
+        res.status(411).json({
+            message: "user not found, error should ideally not happen"
+        })
+        return;
+    }
+
+    res.json({
+        username: user.username,
+        content: content
+    })
+
 })
 
 app.listen(3000);
